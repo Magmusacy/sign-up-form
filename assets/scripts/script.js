2cef2262d55128e872dd43db81d51972effa4fa0
Magmusacy <xpfk.098@gmail.com>
2022-04-02 12:50:02 +0200

    Add more sophisticated validations

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,14 @@
+const INVALID_PSWD_MSG = 'Password must contain: at least 6 characters, 1 upper case character, 1 lowercase character, 1 number and must match password confirmation';
+const VALID_PSWD_MSG = 'Password is correct.'
+const PASSWORD_REGEX = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+
 let password = document.querySelector('#password');
 let passwordConfirmation =  document.querySelector('#password_confirmation');
 let passwords = [password, passwordConfirmation];
 
 function checkPasswordValidity() {
   let values = passwords.map(pswd => pswd.value);
-  return values[0] == values[1] && values[0] != "" && values[1] != "";
+  return values[0] === values[1] && values[0] != "" && values[1] != "" && PASSWORD_REGEX.test(values[0]);
 }
 
 function addInvalidStyle() {
@@ -19,11 +23,11 @@ function addValidStyle() {
 
 function displayPasswordStatus() {
   let passwordStatus = document.querySelector('#password-info');
-  passwordStatus.textContent = "Passwords do not match.";
+  passwordStatus.textContent = INVALID_PSWD_MSG;
   addInvalidStyle();
 
   if (checkPasswordValidity()) {
-    passwordStatus.textContent = "Passwords are valid.";
+    passwordStatus.textContent = VALID_PSWD_MSG;
     addValidStyle();
   }
 }
